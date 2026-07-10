11a1fcfde03cf13af6dadbeab39c2f67ee9b0dea
AnaghaAjidev <anaghaajidev@gmail.com>
2026-07-10 23:20:00 +0530

    Commit new changes

--- src/components/DeleteMission.jsx
+++ src/components/DeleteMission.jsx
@@ -2,29 +2,29 @@ import React from 'react'
 import NavBar from './NavBar'
 
 const DeleteMission = () => {
-  return (
-    <>
-    <NavBar/>
-    <div className="container mt-5">
-      <div className="card shadow p-4 col-md-6 mx-auto">
+    return (
+        <>
+            <NavBar />
+            <div className="container mt-5">
+                <div className="card shadow p-4 col-md-6 mx-auto">
 
-        <h3 className="text-center mb-3">Delete Mission</h3>
+                    <h3 className="text-center mb-3">Delete Mission</h3>
 
-        <label className="form-label">Mission ID</label>
-        <input
-          type="text"
-          className="form-control mb-3"
-          placeholder="Enter Mission ID"
-        />
+                    <label className="form-label">Mission ID</label>
+                    <input
+                        type="text"
+                        className="form-control mb-3"
+                        placeholder="Enter Mission ID"
+                    />
 
-        <button className="btn btn-danger">
-          Delete
-        </button>
+                    <button className="btn btn-danger">
+                        Delete
+                    </button>
 
-      </div>
-    </div>
-    </>
-  )
+                </div>
+            </div>
+        </>
+    )
 }
 
 export default DeleteMission
